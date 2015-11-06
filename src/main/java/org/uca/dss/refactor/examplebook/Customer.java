c872060bcb7490e95ca39f4bd7e6aa447272505c
Daniel Molina <daniel.molina@uca.es>
2015-11-06 10:39:37 +0100

    Update extract Rental::incremFrequentPoints.

--- src/main/java/org/uca/dss/refactor/examplebook/Customer.java
+++ src/main/java/org/uca/dss/refactor/examplebook/Customer.java
@@ -22,19 +22,12 @@ class  Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		
 		for(Rental each: _rentals) {
-			double thisAmount = getAmount(each);
-			totalAmount += thisAmount;
-			
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) 
-					&&
-					each.getDaysRented() > 1) frequentRenterPoints ++;
+			totalAmount += each.getAmount();
+			frequentRenterPoints = each.incremFrequentPoints(frequentRenterPoints);
 			//show figures for this rental
 			result 
 			+= "\t" + each.getMovie().getTitle()+ "\t" +
-					String.valueOf(thisAmount) + "\n";
+					String.valueOf(each.getAmount()) + "\n";
 		}
 		//add footer lines
 		result +=  "Amount owed is " + String.valueOf(totalAmount) +"\n";
@@ -43,29 +36,4 @@ class  Customer {
 
 		return result;
 	}
-	/**
-	 * @param rental
-	 * @return
-	 */
-	private double getAmount(Rental rental) {
-		double thisAmount = 0;
-		//determine amounts for each line
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2)*1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3)*1.5;
-			break;
-		}
-		return thisAmount;
-	}
-
 }
--- src/main/java/org/uca/dss/refactor/examplebook/Rental.java
+++ src/main/java/org/uca/dss/refactor/examplebook/Rental.java
@@ -13,4 +13,47 @@ class Rental {
 	public Movie getMovie() {
 		return _movie;
 	} 
+	
+	/**
+	 * @param rental
+	 * @return
+	 */
+	public double getAmount() {
+		double thisAmount = 0;
+		//determine amounts for each line
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (getDaysRented() > 2)
+				thisAmount += (getDaysRented() - 2)*1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (getDaysRented() > 3)
+				thisAmount += (getDaysRented() - 3)*1.5;
+			break;
+		}
+		return thisAmount;
+	}
+	
+	/**
+	 * @param frequentRenterPoints
+	 * @param each
+	 * @return
+	 */
+	public int incremFrequentPoints(int frequentRenterPoints) {
+		// add frequent renter points
+		frequentRenterPoints ++;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) 
+				&&
+				getDaysRented() > 1) frequentRenterPoints ++;
+		return frequentRenterPoints;
+	}
+
+
+
 }
